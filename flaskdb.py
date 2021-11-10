017ab9eb66c273387b771e1baff4fabb6f368642
MacBook <macbook@MBP-van-MacBook.fritz.box>
2021-11-10 15:11:58 +0100

    flask update2

--- flaskdb.py
+++ flaskdb.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import sqlite3
 import json
+import datetime
 
 app = Flask(__name__)
 
@@ -14,9 +15,9 @@ def result():
 
 def addDb(data):
     obj = json.loads(data)
-    cur.execute('''CREATE TABLE IF NOT EXISTS results (TA1 real, TA2 real, TA1_2 real, TAP real, TB1 real, TB2 real, TB1_2 real, TBP real, TC1 real, TC2 real, TC1_2 real, TCP real, TD1 real, TD2 real, TD1_2 real, TDP real,
+    cur.execute('''CREATE TABLE IF NOT EXISTS results (TIMESTAMP datetime, TA1 real, TA2 real, TA1_2 real, TAP real, TB1 real, TB2 real, TB1_2 real, TBP real, TC1 real, TC2 real, TC1_2 real, TCP real, TD1 real, TD2 real, TD1_2 real, TDP real,
               flow_1 real, flow_2 real, flow_3 real, flow_H2)''')
-    cur.execute("INSERT INTO results VALUES (:TA1, :TA2, :TA1_2, :TAP, :TB1, :TB2, :TB1_2, :TBP, :TC1, :TC2, :TC1_2, :TCP, :TD1, :TD2, :TD1_2, :TDP, :flow_1, :flow_2, :flow_3, :flow_H2)", obj)
+    cur.execute("INSERT INTO results VALUES (:TIMESTAMP, :TA1, :TA2, :TA1_2, :TAP, :TB1, :TB2, :TB1_2, :TBP, :TC1, :TC2, :TC1_2, :TCP, :TD1, :TD2, :TD1_2, :TDP, :flow_1, :flow_2, :flow_3, :flow_H2)", obj)
     con.commit()
     con.close()
 
